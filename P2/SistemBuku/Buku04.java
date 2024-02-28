c434219224afc4400c5349515c70848e40e050e5
Annisa Kurniawati <144205067+annisakrnn@users.noreply.github.com>
2024-02-28 15:15:29 +0700

    P2_Latihan Praktikum_Pertanyaan1_1F_04

--- P2/SistemBuku/Buku04.java
+++ P2/SistemBuku/Buku04.java
@@ -1,12 +1,13 @@
 package SistemBuku;
 
 public class Buku04 {
-    
+
     public String judul, pengarang;
     public int halaman, stok, harga;
 
     public Buku04() {
     }
+
     public Buku04(String jud, String pg, int hal, int stok, int har) {
         judul = jud;
         pengarang = pg;
@@ -26,11 +27,30 @@ public class Buku04 {
     void terjual(int jml) {
         stok -= jml;
     }
+
     void restock(int jml) {
-        stok += jml; 
+        stok += jml;
     }
 
     void gantiHarga(int hrg) {
-        harga = hrg; 
+        harga = hrg;
+    }
+
+    int hitungHargaTotal(int jmlTerjual) {
+        return harga * jmlTerjual;
+    }
+
+    double hitungDiskon(int totalHarga) {
+        if (totalHarga > 150000) {
+            return 0.12 * totalHarga;
+        } else if (totalHarga >= 75000 && totalHarga <= 150000) {
+            return 0.05 * totalHarga;
+        } else {
+            return 0;
+        }
+    }
+
+    int hitungHargaBayar(int totalHarga, double diskon) {
+        return (int) (totalHarga - diskon);
     }
 }
--- P2/SistemBuku/BukuMain04.java
+++ P2/SistemBuku/BukuMain04.java
@@ -2,24 +2,21 @@ package SistemBuku;
 
 public class BukuMain04 {
     public static void main(String[] args) {
-        Buku04 Bk1 = new Buku04();
-        Bk1.judul = "Today Ends Tomorrow Comes";
-        Bk1.pengarang = "Denada Pratiwi";
-        Bk1.halaman = 198;
-        Bk1.stok = 13;
-        Bk1.harga = 71000;
-
-        Bk1.tampilInformasi();
-        Bk1.terjual(5);
-        Bk1.gantiHarga(60000);
-        Bk1.tampilInformasi();
-
-        Buku04 bk2 = new Buku04("Self Reward", "Maheera Ayesha", 160, 29, 59000);
-        bk2.terjual(11);
-        bk2.tampilInformasi();
-
-        Buku04 Annisa = new Buku04("Mariposa", "Luluk HF", 230, 12, 980000);
-        Annisa.terjual(7);
+        Buku04 Annisa = new Buku04();
+        Annisa.judul = "Mariposa";
+        Annisa.pengarang = "luluk HF";
+        Annisa.halaman = 290;
+        Annisa.stok = 1;
+        Annisa.harga = 98000;
         Annisa.tampilInformasi();
+
+        int hargaTotalAnnisa = Annisa.hitungHargaTotal(1);
+        double diskonAnnisa = Annisa.hitungDiskon(98000);
+        int hargaBayarAnnisa = Annisa.hitungHargaBayar(98000, 5);
+        
+        System.out.println("Harga Total: " + hargaTotalAnnisa);
+        System.out.println("Diskon: " + diskonAnnisa);
+        System.out.println("Harga Bayar: " + hargaBayarAnnisa);
+
     }
 }
