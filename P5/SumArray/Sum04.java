2d35f86d1c11450ec7f25711edbb724767abdc4d
Annisa Kurniawati <144205067+annisakrnn@users.noreply.github.com>
2024-03-24 12:19:56 +0700

    P5_Percobaan3_1F_04

---
+++ P5/SumArray/Sum04.java
@@ -0,0 +1,32 @@
+public class Sum04 {
+    int elemen;
+    double keuntungan[], total;
+
+    Sum04(int elemen){
+        this.elemen = elemen;
+        this.keuntungan = new double[elemen];
+        this.total = total;
+
+        this.keuntungan = keuntungan;
+    }
+
+    double totalBF(double arr[]){
+        double total = 0;
+        for(int i=0; i < arr.length; i++){
+            total = total + arr[i];
+        }
+        return total;
+    }
+
+    double totalDC(double arr[], int l, int r){
+        if(l == r){
+            return arr[l];
+        } else if(l < r){
+            int mid = (l + r) / 2;
+            double lsum = totalDC(arr, l, mid - 1);
+            double rsum = totalDC(arr, mid + 1, r);
+            return lsum + rsum + arr[mid];
+        }
+        return 0;
+    }
+}
