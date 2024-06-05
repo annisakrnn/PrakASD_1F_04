0be26f48e0fa8859486656c43ec4fab492cdc5aa
annisakrnn <144205067+annisakrnn@users.noreply.github.com>
2024-06-05 14:20:01 +0700

    P15KEDUA_Percobaan1_Pertanyaan 14_1F_04

---
+++ P15_KEDUA/graphmainn04.java
@@ -0,0 +1,18 @@
+public class graphmainn04 {
+
+        public static void main(String[] args) throws Exception{
+            graph04 gedung = new graph04(6);
+
+    
+            gedung.addEdge(0, 1, 50);
+            gedung.addEdge(0, 2, 100);
+            gedung.addEdge(1, 3, 70);
+            gedung.addEdge(2, 3, 40);
+            gedung.addEdge(3, 4, 60);
+            gedung.addEdge(4, 5, 80);
+            gedung.degree(0);
+            gedung.printGraph();
+            
+        }
+    }
+
---
+++ P15_KEDUA/nodee04.java
@@ -0,0 +1,12 @@
+public class nodee04 {
+    int data;
+    nodee04 prev, next;
+    int jarak;
+
+    nodee04(nodee04 prev, int data, int jarak, nodee04 next){
+        this.prev= prev;
+        this.data=data;
+        this.next=next;
+        this.jarak=jarak;
+}
+}
