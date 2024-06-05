373532fd86dc59a0f64e02e7274be1e18a9c56a5
annisakrnn <144205067+annisakrnn@users.noreply.github.com>
2024-06-05 15:10:54 +0700

    P15KEDUA_Percobaan2_1F_04

--- P15_KEDUA/graphmainn04.java
+++ P15_KEDUA/graphmainn04.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
 public class graphmainn04 {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        graph04 gedung = new graph04(6);
+
+        public static void main(String[] args) throws Exception{
+            Scanner sc = new Scanner(System.in);
+            //graph04 gedung = new graph04(6);
+            GraphMatriks04 gedung = new GraphMatriks04(4);
+
+        //gedung.addEdge(2, 3, 10); 
+        //gedung.addEdge(3, 2, 10); 
+        //gedung.addEdge(3, 4, 10); 
+        //gedung.addEdge(4, 3, 10); 
+
+    
+            gedung.makeEdge(0, 1, 50);
+            gedung.makeEdge(1, 0, 50);
+            gedung.makeEdge(1, 2, 70);
+            gedung.makeEdge(2, 1, 80);
+            gedung.makeEdge(2, 3, 40);
+            gedung.makeEdge(3, 0, 90);
+            gedung.printGraph();
+            System.out.println("Hasil setelah pengahpusan edge");
+            gedung.removeEdge(2, 1);
+            gedung.printGraph();
 
         
-        gedung.addEdge(2, 3, 10);
-        gedung.addEdge(3, 2, 10);
-        gedung.addEdge(3, 4, 10);
-        gedung.addEdge(4, 3, 10);
+        
+        //for (int i = 0; i < 2; i++) {
+            //System.out.print("Masukkan gedung asal: ");
+            //int asal = sc.nextInt();
+            //System.out.print("Masukkan gedung tujuan: ");
+            //int tujuan = sc.nextInt();
 
-        for (int i = 0; i < 2; i++) {
-            System.out.print("Masukkan gedung asal: ");
-            int asal = sc.nextInt();
-            System.out.print("Masukkan gedung tujuan: ");
-            int tujuan = sc.nextInt();
+        //if (gedung.cek(asal, tujuan)) {
+            //System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
+        //} else {
+            //System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
+        //}
 
-            if (gedung.cek(asal, tujuan)) {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
-            } else {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
-            }
-            System.out.println();
+        //sc.close();
+
+            
         }
-        sc.close();
     }
-}
+
+
